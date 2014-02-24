ac78a34d6ef5c0440f6c08b6bccdc1f639b8f8a9
Chris Arriola <arriolac279@gmail.com>
2014-02-23 23:56:08 -0800

    Adding apache license.

--- emailautocompletetextview/src/main/java/com/greenhalolabs/emailautocompletetextview/EmailCompleteTextViewPlus.java
+++ emailautocompletetextview/src/main/java/com/greenhalolabs/emailautocompletetextview/EmailCompleteTextViewPlus.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2013, Green Halo Labs LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with the License.
+ * You may obtain a copy of the License in the LICENSE file, or at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" 
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language 
+ * governing permissions and limitations under the License. 
+ */
 
 package com.greenhalolabs.emailautocompletetextview;
 
@@ -112,7 +124,9 @@ public class EmailCompleteTextViewPlus extends AutoCompleteTextView implements
 
         // Set clear button
         final Drawable[] drawables = getCompoundDrawables();
-        mTappableDrawable = ((drawables != null && drawables[2] != null) ? drawables[2] : getDefaultClearButton(context));
+        mTappableDrawable = ((drawables != null && drawables[2] != null)
+                ? drawables[2]
+                : getDefaultClearButton(context));
         if (mTappableDrawable != null) {
             mTappableDrawable.setBounds(0,
                                         0,
--- emailautocompletetextview/src/main/java/com/greenhalolabs/emailautocompletetextview/util/AccountUtil.java
+++ emailautocompletetextview/src/main/java/com/greenhalolabs/emailautocompletetextview/util/AccountUtil.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2013, Green Halo Labs LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with the License.
+ * You may obtain a copy of the License in the LICENSE file, or at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package com.greenhalolabs.emailautocompletetextview.util;
 
 import android.accounts.Account;
@@ -11,9 +24,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by chris on 2/23/14.
- */
 public class AccountUtil {
 
     /**
